d0798508fb8778ae79784828ab7852e094247583
lavsam-dev <lavsam@gmail.com>
2022-02-20 23:14:27 +0400

    #3 ListItem Card Box Image

--- app/src/main/java/lavsam/dev/necocompouse/MainActivity.kt
+++ app/src/main/java/lavsam/dev/necocompouse/MainActivity.kt
@@ -3,53 +3,71 @@ package lavsam.dev.necocompouse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import lavsam.dev.necocompouse.ui.theme.NECOcompouseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.5f)
-            ) {
-                Column(
+            Column() {
+                ListItem(name = "Андрей Львов", prof = "Программист")
+                ListItem(name = "Андрей Львов", prof = "Программист")
+                ListItem(name = "Андрей Львов", prof = "Программист")
+                ListItem(name = "Андрей Львов", prof = "Программист")
+                ListItem(name = "Андрей Львов", prof = "Программист")
+            }
+        }
+    }
+}
+
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.lav_ava),
+                    contentDescription = "lav_ava",
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .background(Color.Blue)
-                        .fillMaxWidth(0.5f)
-                        .fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text("Hello")
-                    Text("Girona")
-                    Text("Madrid")
-                }
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
                 Column(
-                    modifier = Modifier
-                        .background(Color.Magenta)
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    modifier = Modifier.padding(start = 16.dp)
                 ) {
-                    Text("Hello")
-                    Text("Girona")
-                    Text("Madrid")
+                    Text(text = name)
+                    Text(text = prof)
                 }
             }
         }
     }
+
 }
 
